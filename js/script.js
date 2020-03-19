0b121ef1e9b76a4d0bcdb00c14fbb3ee43384d9f
9MrDrago9 <munisjon_98@mail.ru>
2020-03-19 22:14:21 +0300

    validate

--- js/script.js
+++ js/script.js
@@ -6,17 +6,16 @@ var close = popup.querySelector(".button-close");
 var form = popup.querySelector("form");
 var name = popup.querySelector("[name=name]");
 
-link.addEventListener("click", function (evt) {
+link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  // name.focus();
-  });
+});
 
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-  });
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
 
-form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-  });
+form.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+});
