972e8591871afb6a49c849f6fb9eca2c6d6c8554
9MrDrago9 <munisjon_98@mail.ru>
2020-03-21 20:20:50 +0300

    проверка

--- js/script.js
+++ js/script.js
@@ -4,18 +4,36 @@ var popup = document.querySelector(".write-us");
 var close = popup.querySelector(".button-close");
 
 var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
+var yourName = popup.querySelector("[name=yourName]");
+var yourEmail = popup.querySelector("[name=yourEmail]");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  yourName.focus();
 });
 
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
-  evt.preventDefault();
+  if (!yourName.value || !yourEmail.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
